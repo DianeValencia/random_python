e152ee4f151b0b2d19ec19f2d092f985dd06e415
D <dianeangelinevalencia@gmail.com>
2025-07-09 17:14:25 +0800

    Update random.py

--- random.py
+++ random.py
@@ -1,7 +1,18 @@
-print ("Hello World")
+print ("Hello Github")
 
 name = "Diane Angeline M. Valencia"
 birthday = "May 19, 2003"
 age = 22
-
 #integers = whole numbers; float = decimals
+is_CS_student = True
+#boolean value = True or False
+
+print(name)
+print(birthday)
+print(age)
+print(is_CS_student)
+
+print("Name: ", name)
+print("Birthday: ", birthday)
+print("Age: ", age)
+print("Computer Science student? ", is_CS_student)
