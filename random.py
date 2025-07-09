719d1415703ac512325e52e5074465c858ebc841
D <dianeangelinevalencia@gmail.com>
2025-07-09 17:55:52 +0800

    Update random.py

--- random.py
+++ random.py
@@ -1,3 +1,5 @@
+#NOTES FROM UDEMY COURSE: PYTHON MASTERY: 100 DAYS, 100 PROJECTS
+
 print ("Hello Github")
 
 name = "Diane Angeline M. Valencia"
@@ -61,4 +63,64 @@ while count <5:
   count += 1
 
       
+print("\n FOR LOOP \n")
 
+my_games = ["Valorant", "Overcooked", "It Takes Two"]
+for game in my_games:
+  print(game)
+
+print("\n LIST \n")
+
+print(my_games[0])
+print(my_games[2])
+print(my_games[1])
+
+#append means add to the list
+my_games.append("Flappy Bird")
+print(my_games)
+
+my_games[3] = "Candy Crush"
+print(my_games)
+
+print("\n FUNCTIONS \n")
+
+def greet(temp_name):
+    print("Hello, " + temp_name + " This is a practice!")
+
+greet("Dee!")
+greet("Diane!")
+greet("Angeline!")
+
+def multiply(a, b):
+    return a * b
+
+result = multiply(5, 19)
+print(result)
+print("Result is ", result)
+
+
+print()
+print()
+print("\n Mini Project: Number Guessing Game \n")
+
+import random 
+
+secret_num = random.randint(1,10)#randint means random integer
+attempts = 5
+
+print("What number I am thinking between 1-10?")
+
+while attempts > 0:
+    guess = int(input("Take a Guess: "))
+    if guess == secret_num:
+        print("You read my mind!")
+    elif guess < secret_num:
+        print("Nah. Try higher.")
+    else:
+        print("Hmm... Try lower.")
+    attempts -= 1
+
+if attempts == 0:
+    print("That was a good try! But you can't guess anymore.")
+    print("For your peace of mind, the secret numberr was " + secret_num + "!")
+    
